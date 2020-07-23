f27cb3427c98a03b41e3649556592714c10a1647
Gaushik <gaushik16@gmail.com>
2020-07-23 10:14:01 -0500

    Day 16

---
+++ Python/CheckSubset.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+for i in range(int(input())):
+    _, a = input(), set(map(int, input().split()))
+    _, b = input(), set(map(int, input().split()))
+    print(a.issubset(b))
